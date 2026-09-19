5a9b5c1f6d532e28f10058635799f5c757656e36
AvdhutKulkarni0308 <avdhutk2005@gmail.com>
2026-09-19 16:25:51 +0530

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,16 @@ int main()
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
-  while (true)
-  {
+  while (true){
     
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
     if (input == "exit") {
       break;
-    }
+    } else if (input.substr(0,5) == "echo"){
+      std::cout << "input.substr(5)" << std::endl;
+    }    
     else
     std::cout << input << ": command not found" << std::endl;
   }
